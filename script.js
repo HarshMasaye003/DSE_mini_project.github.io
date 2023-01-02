4e4e2e09e8f2a7f01a1dd44573fe5996796d9b6a
HarshMasaye06 <vu2s2223003@pvppcoe.ac.in>
2023-01-02 19:45:10 +0530

    2jan2023

---
+++ script.js
@@ -0,0 +1,41 @@
+const checkbox = document.getElementById('checkbox');
+
+checkbox.addEventListener('change', () => {
+    document.body.classList.toggle('dark');
+});
+
+let HTMLBox = document.getElementById("HTMLBox");
+let HTMLButton = document.getElementById("HTMLButton");
+
+HTMLButton.onclick = function () {
+    HTMLBox.select();
+    document.execCommand("copy");
+    HTMLButton.innerText = "Codes Copied"
+}
+
+window.onload = () => {
+    const tab_switchers = document.querySelectorAll('[data-switcher]');
+
+    for (let i = 0; i < tab_switchers.length; i++) {
+        const tab_switcher = tab_switchers[i];
+        const page_id = tab_switcher.dataset.tab;
+
+        tab_switcher.addEventListener('click', () => {
+            document.querySelector('.tabs .tab.is-active').classList.remove('is-active');
+            tab_switcher.parentNode.classList.add('is-active');
+
+            SwitchPage(page_id);
+        });
+    }
+}
+
+function SwitchPage(page_id) {
+    console.log(page_id);
+
+    const current_page = document.querySelector('.pages .page.is-active');
+    current_page.classList.remove('is-active');
+
+    const next_page = document.querySelector(`.pages .page[data-page="${page_id}"]`);
+    next_page.classList.add('is-active');
+}
+
